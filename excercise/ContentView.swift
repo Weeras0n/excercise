11ed7a145d7a294fdd455abc53a0c83eecb2fd2d
1 <pakkawat.phupha2552@gmail.com>
2025-10-06 08:56:48 -0700

    Initial Commit

---
+++ excercise/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  excercise
+//
+//  Created by 1 on 6/10/2568 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ excercise/excerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  excerciseApp.swift
+//  excercise
+//
+//  Created by 1 on 6/10/2568 BE.
+//
+
+import SwiftUI
+
+@main
+struct excerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
